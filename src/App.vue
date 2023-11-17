6c92032491f491099c20094bbb6b2b39f35d6943
mac <yujizhou@zces.cn>
2023-11-17 18:05:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>{{ a }}</h2>
+    <h3>{{ b }}</h3>
+    <h3>{{ c }}</h3>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            a: "hello vue3"
+            b: true,
+            c: false,
+        }
+    }
+}
+</script>
